a96cdc37d36aa22eaae6e3a12b7db0fb3619c957
Mai-katou <95422650+Mai-katou@users.noreply.github.com>
2022-05-16 22:13:47 +0900

    [Update]Java型の学習

---
+++ JavaWcp2/src/Chapter05.java
@@ -0,0 +1,25 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+	long l=32;
+	System.out.println(1);
+
+	double d=1234.567;
+	System.out.println(d);
+	
+	boolean b=true;
+	System.out.println(b);
+	
+	Float f =0.1234f;
+	System.out.println(f);
+	
+	Integer i=54;
+	System.out.println(i);
+	
+	Character c='ぬ';
+	System.out.println(c);
+	
+	int[] array= {1,2,3,4,};
+	System.out.println(array[3]);
+	}
+}
